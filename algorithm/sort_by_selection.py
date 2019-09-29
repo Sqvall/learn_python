cc7dd2a8ee02961fc6defa1304b1e92194f73816
sqvall <masster1987@inbox.ru>
2019-09-29 23:38:46 +0300

    *

---
+++ algorithm/sort_by_selection.py
@@ -0,0 +1,19 @@
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def smallest_sort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+print(smallest_sort([1, 13, 3, 53, 9, 111]))
