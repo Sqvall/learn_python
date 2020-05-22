6378871953d822f2ad9099dbb1955f1fbf6fdd29
sqvall <masster1987@inbox.ru>
2020-05-22 22:46:32 +0300

    *

--- algorithm/basis_cosine_similarity.py
+++ algorithm/basis_cosine_similarity.py
@@ -1,9 +1,11 @@
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-u1 = [0, 0, 0, 1]
-u2 = [0, 0, 0, 1]
-vbl = [0, 0, 0, 2]
+u1 = [0, 1, 1, 0]
+u2 = [0, 0, 1, 1]
+vbl = [0, 0, 2, 0]
 vb = np.array(vbl)
 
 print(round(cosine_similarity(np.array([vb, np.array(u1)]))[0][1]*100, 2))
 print(round(cosine_similarity(np.array([vb, np.array(u2)]))[0][1]*100, 2))
+
+print(cosine_similarity(np.array([vb, np.array(u2)])))
